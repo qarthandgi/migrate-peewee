56f60ec89e672a678a98a1fab36853f51f44d59d
rwrigley <ryan@servicefusion.com>
2019-08-27 15:39:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="migrate-peewee",
-	version="0.0.4",
+	version="0.0.5",
 	description="Database Migrator for Peewee",
 	url="https://github.com/ServiceF/migrate-peewee.git",
 	py_modules=["migrate_peewee"],
